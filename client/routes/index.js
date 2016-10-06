4e6f8ae5b6acde582c08392769d0a461c3963709
luck_zr <529130510@qq.com>
2016-10-06 09:55:58 +0800

    init first list

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import {Route, IndexRoute} from "react-router";
-import Layout from "../page/Layout";
-import Home from "../page/Home";
-import About from "../page/About";
+import React from "react"
+import {Route, IndexRoute} from "react-router"
+import Layout from "../page/Layout"
+import Repository from "../page/Repository"
+import RepositoryDetail from '../page/Repository_detail'
 
 export default (
   <Route path="/" component={Layout}>
-    <IndexRoute component={Home}/>
-    <Route path="home" component={Home}></Route>
-    <Route path="about" component={About}></Route>
+    <IndexRoute component={Repository}/>
+    <Route path="repository" component={Repository}>
+    </Route>
+    <Route path="repository/detail/:id" component={RepositoryDetail} />
   </Route>
 )
--- db.js
+++ db.js
@@ -5,8 +5,10 @@
 export default {
   repository: [
     {
+      id: 0,
       name: '维保平台'
     },{
+      id: 1,
       name: '万达系统'
     }
   ]
