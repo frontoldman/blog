264908bc8982cd502b2aa41667150bed0f98cc4b
luck_zr <529130510@qq.com>
2016-10-28 13:51:14 +0800

    fix host

--- webpack/client/index.js
+++ webpack/client/index.js
@@ -1,10 +1,10 @@
-import koa from "koa";
-import webpack from "webpack";
-import koaWebpackDevMiddleware from "koa-webpack-dev-middleware";
-import koaWebpackHotMiddleware from "koa-webpack-hot-middleware";
+import koa from 'koa'
+import webpack from 'webpack'
+import koaWebpackDevMiddleware from 'koa-webpack-dev-middleware'
+import koaWebpackHotMiddleware from 'koa-webpack-hot-middleware'
 import {client} from '../../config'
-import webpackClientConfig from "./client.config.js";
-import webpackServerConfig from "./server.config.js";
+import webpackClientConfig from './client.config.js'
+import webpackServerConfig from './server.config.js'
 
 var app = koa()
 var compile = webpack(webpackClientConfig)
@@ -15,13 +15,12 @@ app.use(koaWebpackDevMiddleware(compile, {
 }))
 app.use(koaWebpackHotMiddleware(compile))
 
-
 app.listen(client.port, '0.0.0.0', error => {
   if (error) {
     console.log(error)
-    return;
+    return
   }
-  console.log(`listening on ${client.port}`);
+  console.log(`listening on ${client.port}`)
 })
 
 
