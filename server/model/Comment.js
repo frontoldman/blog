11acadeef26e379d39924055e8bcff574be97a55
luck_zr <529130510@qq.com>
2017-01-24 16:16:39 +0800

    新增model relay支持

--- server/model/Comment.js
+++ server/model/Comment.js
@@ -8,6 +8,7 @@ var Schema = mongoose.Schema
 var CommentSchema = new Schema({
   content: String,
   article: {type: Schema.Types.ObjectId, ref: 'Article'},
+  reply: {type: Schema.Types.ObjectId, ref: 'Comment'},
   creater: {type: Schema.Types.ObjectId, ref: 'User'},
   createTime: {type: Date, default: Date.now},
   updateTime: {type: Date, default: Date.now}
